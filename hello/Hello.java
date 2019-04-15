ed98f3eee1f5b25fd87967cf72306c933ee16923
Dansytov1 <evmenova@inbox.ru>
2019-04-15 20:05:28 +0300

    My first commit

---
+++ hello/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+   public static void main(String[] args) {
+      System.out.println("Hello World");
+      System.out.println("I LOVE JAVA");
+      System.out.println(COLOR);
+           
+   }
+//System.out.println("Hello World");
+	static String COLOR = "RED";
+
+}
