208b273812d516d79984ff8d9518b80abbd43f0e
Dansytov1 <evmenova@inbox.ru>
2019-04-16 17:02:23 +0300

    My homework by Lesson 3

--- Homework3/module1/src/Task3_1.java
+++ Homework3/module1/src/Task3_1.java
@@ -1,17 +1,24 @@
 public class Task3_1 {
         public static void main(String[] args) {
-            int s = 2500;
+            int s = 11840000;
             int sec;
             int m;
-            int min, h, day, week, d;
+            int min, h, day, w, d,hour;
+
             sec = s % 60;
+
             m = (s -sec) / 60;
             min = m % 60;
+
             h = (m -min) / 60;
-            day = h%24;
-            d=(h-day)/24;
-            week=d%7;
-            System.out.println(week+" недель, "+day+" дней, "+h+" часов, " + min + " минут, " + sec + " секунд");
+            hour=h%24;
+
+            d=(h-hour)/24;
+            day = d%7;
+
+            w=(d-day)/7;
+
+            System.out.println(w+" недель, "+day+" дней, "+hour+" часов, " + min + " минут, " + sec + " секунд");
 
         }
     }
